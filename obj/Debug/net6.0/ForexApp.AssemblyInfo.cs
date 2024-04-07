9c62a24fa809b06f907ef1e8d5261902d356b563
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2024-04-07 17:18:42 +0100

    Finalização do Estilo no Home

--- obj/Debug/net6.0/ForexApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/ForexApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71327fbc0d7a5d8be637ce73b674f4ed87a5800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1581a9de1c6f3ade25bafd04ba051e1f9d4a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
